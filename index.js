52b6a5c959c79903c53e84c2e5377970491be88e
frantic1048 <archer@frantic1048.com>
2016-08-25 18:19:52 +0800

    thow a LinterError when warning/error in result

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const lint = require('sass-lint')
 
+function LinterError (message) {
+  this.name = 'LinterError'
+  this.message = message || ''
+}
+
+LinterError.prototype = Object.create(Error.prototype)
+
 module.exports = function plugin () {
   this.sasslint = function sassLint (inputConfig) {
     const config = inputConfig || {}
     const options = config.options || {}
     const configPath = config.configPath
+
     /**
      * config contains same interface as sassLint.lintFiles()
      * @see: https://github.com/sasstools/sass-lint/blob/develop/index.js#L172-L182
@@ -19,6 +27,16 @@ module.exports = function plugin () {
 
       /** print it out */
       lint.outputResults(results, options, configPath)
+
+      /**
+       * if any warning/error in results
+       * throw a LinterError to fail the task
+       */
+      const errorCount = results.reduce(function (sum, r) { return sum + r.errorCount }, 0)
+      const warningCount = results.reduce(function (sum, w) { return sum + w.warningCount }, 0)
+      if (warningCount > 0 || errorCount > 0) {
+        throw new LinterError(errorCount + ' errors and ' + warningCount + ' warnings found.')
+      }
     })
   }
 }
